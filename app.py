6b3b552d5515d244cfbaa345034e3379f0da135b
SYARIZAM15 <chansyarizam@gmail.com>
2026-05-05 16:41:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jpkn_sandakan_secret'
+app.config['SECRET_KEY'] = 'jpkn_sandakan_2026'
 
-# Menggunakan SQLite (Fail tempatan) untuk mengelakkan ralat sambungan server
-# Ini akan mencipta fail bernama 'database.db' secara automatik
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# Menggunakan SQLite untuk mengelakkan ralat sambungan PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///assets.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login' # Memastikan user ke /login jika tiada akses[cite: 1]
 
 # --- MODELS ---
 class User(UserMixin, db.Model):
@@ -33,11 +32,12 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 # --- ROUTES ---
+
 @app.route('/')
-def home():
-    if current_user.is_authenticated:
-        return redirect(url_for('assets'))
-    return redirect(url_for('login'))
+@login_required # Menyekat akses tanpa login[cite: 1]
+def index():
+    all_assets = Asset.query.all()
+    return render_template('index.html', assets=all_assets)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -45,28 +45,23 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         
-        # Log masuk default untuk JPKN
+        # Log masuk admin JPKN
         if username == 'admin' and password == 'jpkn123':
-            user = User.query.filter_by(username=username).first()
+            user = User.query.filter_by(username='admin').first()
             if not user:
-                user = User(username=username, password=password)
+                # Mencipta user admin jika belum wujud dalam database baru
+                user = User(username='admin', password='password')
                 db.session.add(user)
                 db.session.commit()
             login_user(user)
-            return redirect(url_for('assets'))
+            return redirect(url_for('index'))
         else:
-            flash('ID atau Kata Laluan Salah')
+            flash('ID atau Kata Laluan Salah!')
     return render_template('login.html')
 
-@app.route('/assets')
-@login_required
-def assets():
-    all_assets = Asset.query.all()
-    return render_template('index.html', assets=all_assets)
-
 @app.route('/add', methods=['GET', 'POST'])
-@login_required
-def add_asset():
+@login_required # Menyekat akses tanpa login[cite: 1]
+def add():
     if request.method == 'POST':
         new_asset = Asset(
             model=request.form.get('model'),
@@ -76,7 +71,7 @@ def add_asset():
         )
         db.session.add(new_asset)
         db.session.commit()
-        return redirect(url_for('assets'))
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 @app.route('/logout')
@@ -87,5 +82,5 @@ def logout():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all() # Mencipta database.db secara automatik
+        db.create_all() # Mencipta fail assets.db secara automatik
     app.run(debug=True)
